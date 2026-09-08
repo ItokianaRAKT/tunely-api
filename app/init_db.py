e77fd369fd732dc544ae11aaa437b9a1c1f145ed
Itokiana <itokianarakt@gmail.com>
2026-09-08 11:30:09 +0300

    chore: mount new routers and update init_db + requirements

--- app/init_db.py
+++ app/init_db.py
@@ -1,4 +1,4 @@
 from app.database import engine, Base
-from app.models import Playlist
+from app.models import Playlist, Track, PlaylistTrack, Room, Participant, TrackProposal, Vote
 
 Base.metadata.create_all(engine)
